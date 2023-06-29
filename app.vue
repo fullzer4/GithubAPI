79e5abbdaa1aa203161ae9e5f309e1ed6a554254
fullzer4 <gabrielpelizzaro@gmail.com>
2023-06-29 08:34:28 -0300

    test

---
+++ app.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <h1> Get github users </h1>
+    <input type="text" v-model="userName" placeholder="github username">
+    <button @click="fetchUser">Buscar</button>
+
+    <UserInfo/>
+  </div>
+</template>
+
+<script>
+
+import UserInfo from "./components/UserInfo.vue"
+
+export default {
+  components: {
+    UserInfo
+  },
+  data() {
+    return {
+      userName: ''
+    };
+  },
+  methods: {
+    fetchUser() {
+      this.$refs.UserInfo.getGithubUser(this.userName);
+    }
+  }
+};   
+}
+</script>
---
+++ components/UserInfo.vue
@@ -0,0 +1,29 @@
+<template>
+  <div v-if="user">
+  </div>
+</template>
+
+<script>
+
+import axios from "axios"
+
+export default {
+  data() {
+    return {
+      user: null
+    };
+  },
+  methods: {
+    getGithubUser(name) {
+      axios.get(`https://api.github.com/users/${name}`)
+        .then(response => {
+          this.user = response.data;
+          console.log(this.user)  
+        })
+        .catch(error => {
+          console.error(error);
+        });
+    }
+  }
+};
+</script>
